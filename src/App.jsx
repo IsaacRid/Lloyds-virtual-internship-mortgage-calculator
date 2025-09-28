fc48a05aaecff6618b7a3fde4bf98aa32b5945a0
IsaacRid <isaac.rideal@gmail.com>
2025-09-28 23:58:31 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Layout from './components/layouts/Layout'
+import './App.css'
+
+function App() {
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default App
